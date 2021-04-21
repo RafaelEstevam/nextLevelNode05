51855ea714845ae196884a348751869aa454e049
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2021-04-21 18:02:48 -0300

    documentação

--- src/routes.ts
+++ src/routes.ts
@@ -11,46 +11,59 @@ const usersController = new UsersController();
 const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create, () => {
-    /* #swagger.tags = ['Settings']
-        #swagger.description = 'Endpoint to add a settings.' */
+    /*  
+        #swagger.tags = ['Settings']
+        #swagger.description = 'Endpoint to add a settings.'
+    */
 
-    /*  #swagger.parameters['obj'] = {
+    /*  
+        #swagger.parameters['obj'] = {
             in: 'body',
             description: 'Settings.',
             required: true,
             schema: { $ref: "#/definitions/AddSettings" }
-    } */
+        } 
+    */
 });
 
 routes.post("/users", usersController.create, () => {
-    /* #swagger.tags = ['Users']
-        #swagger.description = 'Endpoint to add a user.' */
+    /*  
+        #swagger.tags = ['Users']
+        #swagger.description = 'Endpoint to add a user.'
+    */
 
-    /*  #swagger.parameters['obj'] = {
+    /*  
+        #swagger.parameters['obj'] = {
             in: 'body',
             description: 'Users.',
             required: true,
             schema: { $ref: "#/definitions/AddUser" }
-    } */
+        } 
+    */
 });
 
 routes.post("/messages", messagesController.create, () => {
-    /*  #swagger.auto = false
+    /*  
+        #swagger.auto = false
         #swagger.tags = ['Messages']
         #swagger.path = '/messages'
         #swagger.method = 'post'
-        #swagger.description = 'Endpoint to add a message.' */
+        #swagger.description = 'Endpoint to add a message.'
+    */
 
-    /*  #swagger.parameters['obj'] = {
+    /*  
+        #swagger.parameters['obj'] = {
             in: 'body',
             description: 'Body Message',
             required: true,
             schema: { $ref: "#/definitions/AddMessage" }
-    } */
+        }
+    */
 });
 
 routes.get("/messages/:id", messagesController.index, () => {
-    /*  #swagger.auto = false
+    /*  
+        #swagger.auto = false
         #swagger.tags = ['Messages']
         #swagger.path = '/messages/{id}'
         #swagger.method = 'get'
@@ -58,7 +71,8 @@ routes.get("/messages/:id", messagesController.index, () => {
         #swagger.messages = ["application/json"]
     */
 
-    /*  #swagger.parameters['id'] = {
+    /*  
+        #swagger.parameters['id'] = {
             in: 'path',
             description: 'User ID',
             required: true,
@@ -66,7 +80,5 @@ routes.get("/messages/:id", messagesController.index, () => {
         }
     */
 });
-
-
 
 export {routes};
